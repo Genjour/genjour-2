ae3d7c569be9ea177cf9ca30aa3e6f5f0ff02c34
Injoker <amitraj.1996@gmail.com>
2017-11-09 23:26:00 +0530

    hash tags added

--- front-end/src/app/components/profile/profile.component.ts
+++ front-end/src/app/components/profile/profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
+import {MatChipInputEvent} from '@angular/material';
+import {ENTER} from '@angular/cdk/keycodes';
+const COMMA = 188;
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -19,4 +22,43 @@ export class ProfileComponent implements OnInit {
       {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
       {text: 'five', cols: 1, rows: 1, color: '#DDFDF1'},
     ];
+
+
+      visible: boolean = true;
+      selectable: boolean = true;
+      removable: boolean = true;
+      addOnBlur: boolean = true;
+
+      // Enter, comma
+      separatorKeysCodes = [ENTER, COMMA];
+
+      tags = [
+        { name: 'Genjour' },
+      ];
+
+
+      add(event: MatChipInputEvent): void {
+        let input = event.input;
+        let value = event.value;
+
+        // Add our person
+        if ((value || '').trim()) {
+          this.tags.push({ name: value.trim() });
+        }
+
+        // Reset the input value
+        if (input) {
+          input.value = '';
+        }
+      }
+
+      remove(tag: any): void {
+        let index = this.tags.indexOf(tag);
+
+        if (index >= 0) {
+          this.tags.splice(index, 1);
+        }
+      }
+
+
 }
