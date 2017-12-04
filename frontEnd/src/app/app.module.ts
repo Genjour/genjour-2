a6f71de074b6475fec5df5292747c3f15ca55178
Injoker <amitraj.1996@gmail.com>
2017-12-04 18:48:02 +0530

    basic profile, login, feeds, genjourist

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -8,11 +8,14 @@ import { LoginComponent } from './components/login/login.component';
 import { FeedsComponent } from './components/feeds/feeds.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleComponent } from './components/article/article.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 const appRoutes: Routes = [
   {path: '', component: LoginComponent },
   {path: 'feeds', component: FeedsComponent },
   {path: 'genjourist', component: ProfileComponent },
+  {path: 'article', component:ArticleComponent},
 ]
 
 @NgModule({
@@ -21,7 +24,9 @@ const appRoutes: Routes = [
     NavbarComponent,
     LoginComponent,
     FeedsComponent,
-    ProfileComponent
+    ProfileComponent,
+    ArticleComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
